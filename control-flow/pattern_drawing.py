e608741bbf57078baee3081c5ae87a73227f83dc
mohamed <mohamedhussein6500@gmail.com>
2025-07-20 12:44:46 +0300

    Update pattern_drawing.py with nested while loops

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -4,5 +4,9 @@ rows = int(input("Enter the number of rows: "))
 i = 1
 
 while i <= rows:
-    print("*" * i)
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j += 1
+    print()
     i += 1
