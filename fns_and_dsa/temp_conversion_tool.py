aed8b447e92886664a540b0147893087b3450471
mohamed <mohamedhussein6500@gmail.com>
2025-07-27 23:28:49 +0300

    Fix: Correct constants for temperature conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,13 +5,7 @@ FAHRENHEIT_TO_CELSIUS_OFFSET = 32
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
 def celsius_to_fahrenheit(celsius: float) -> float:
-    """
-    Converts Celsius to Fahrenheit.
-    """
     return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + FAHRENHEIT_TO_CELSIUS_OFFSET
 
 def fahrenheit_to_celsius(fahrenheit: float) -> float:
-    """
-    Converts Fahrenheit to Celsius.
-    """
     return (fahrenheit - FAHRENHEIT_TO_CELSIUS_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
