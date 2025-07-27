e5fdf999dc5d484f224f6bc18b128f9b386cd82a
mohamed <mohamedhussein6500@gmail.com>
2025-07-27 23:48:18 +0300

    Fix: use constants as expected for autograder in temp_conversion_tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
-# Global conversion factors - MUST BE DEFINED EXACTLY LIKE THIS
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+# Global conversion factors - must be exactly as specified
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 FREEZING_POINT_OFFSET = 32
 
 def convert_to_celsius(fahrenheit):
