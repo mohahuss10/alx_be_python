d68562b66f2564704ea7ef467eb40f6e175dc978
mohamed <mohamedhussein6500@gmail.com>
2025-08-09 10:15:34 +0300

    Add Task 0: Book class with magic methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,17 +1,18 @@
 class Book:
-    def __init__(self,title,author,year):
-        """constructor to initialize the book's title, author and year"""
-        self.title =  title
+    def __init__(self, title, author, year):
+        """Constructor to initialize the book's title, author, and year"""
+        self.title = title
         self.author = author
         self.year = year
 
     def __del__(self):
-        """destructor to indicate when a book instance is deleted"""
-        print(f"Deleting{self.title}")
+        """Destructor to indicate when a book instance is deleted"""
+        print(f"Deleting {self.title}")
 
     def __str__(self):
-        """Readable string represantation for humans"""
-        return f"{self.title} by {self.autor}, published in {self.year}"
-    
+        """Readable string representation for humans"""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
     def __repr__(self):
-        return f"Book ({self.title} by {self.autor}, published in {self.year})"
+        """Unambiguous string representation for developers"""
+        return f"Book({self.title!r}, {self.author!r}, {self.year!r})"
