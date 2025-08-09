2e35704c61a491f8e9762f33bb3f8efa15187acf
mohamed <mohamedhussein6500@gmail.com>
2025-08-09 10:43:02 +0300

    Second task - library main file and updates

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,7 +1,8 @@
 class Book:
-    def __init__(self,title,author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
+
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
@@ -10,21 +11,19 @@ class EBook(Book):
     def __init__(self, title, author, file_size):
         """EBook with additional file size attributes"""
         super().__init__(title, author)  # call base constructor
-        self.file_size = file_size # in mb
+        self.file_size = file_size  # in mb
 
     def __str__(self):
-        return f"Book:{self.title} by {self.author} , file size:{self.file_size}"
+        return f"Book: {self.title} by {self.author}, file size: {self.file_size}"
 
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title, author) # call base constructor
+        super().__init__(title, author)  # call base constructor
         self.page_count = page_count
 
-    
     def __str__(self):
-        return f"PrintBook:{self.title}, by {self.author}, pzages:{self.page_count}"
-    
+        return f"PrintBook: {self.title} by {self.author}, pages: {self.page_count}"
 
 
 class Library:
@@ -32,13 +31,12 @@ class Library:
         """Library containing a collection of books"""
         self.books = []
 
-    def add_book(self,book):
+    def add_book(self, book):
         """add a book to the library"""
         if isinstance(book, Book):
             self.books.append(book)
         else:
             raise TypeError("only Book instances can be added to the library")
-        
 
     def list_books(self):
         """List all the books in the library"""
