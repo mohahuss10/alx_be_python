5f7e0640be9fbd194b6aac987df304e82ace2ba5
mohamed <mohamedhussein6500@gmail.com>
2025-08-09 10:48:55 +0300

    Second task - library main file and updates

--- oop/library_system.py
+++ oop/library_system.py
@@ -9,36 +9,29 @@ class Book:
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        """EBook with additional file size attributes"""
-        super().__init__(title, author)  # call base constructor
-        self.file_size = file_size  # in mb
+        super().__init__(title, author)
+        self.file_size = file_size
 
     def __str__(self):
-        return f"Book: {self.title} by {self.author}, file size: {self.file_size}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title, author)  # call base constructor
+        super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, pages: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
     def __init__(self):
-        """Library containing a collection of books"""
         self.books = []
 
     def add_book(self, book):
-        """add a book to the library"""
-        if isinstance(book, Book):
-            self.books.append(book)
-        else:
-            raise TypeError("only Book instances can be added to the library")
+        self.books.append(book)
 
     def list_books(self):
-        """List all the books in the library"""
         for book in self.books:
             print(book)
