5d1d0eae9a307efefdbc60d9e2ae4609c1b4f0a4
mohamed <mohamedhussein6500@gmail.com>
2025-08-09 11:44:15 +0300

    fourth task - static

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -3,12 +3,9 @@ class Calculator:
 
     @staticmethod
     def add(a, b):
-        """Return the sum of two numbers (no access to class or instance)."""
         return a + b
-    
+
     @classmethod
     def multiply(cls, a, b):
-        """Return the product of two numbers and show class attributes"""
-        print (f"calculation type:{cls.calculation_type}")
-
-        return a*b
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b  # return instead of print
